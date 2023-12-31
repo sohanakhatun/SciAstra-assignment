5f4aa500740a8d7b4cfa804816a9ab33033871ec
sohanakhatun <sohanakhatun16@gmail.com>
2023-12-31 19:26:50 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily:{
+      Poppins:"'Poppins', sans-serif",
+      Inter:"'Inter', sans-serif",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
